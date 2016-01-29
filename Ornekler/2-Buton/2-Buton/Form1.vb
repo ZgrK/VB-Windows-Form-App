4a89062e8c79d84181d1b6fb8fac268a5a1125f9
Özgür KARALAR <zgr@zgrkaralar.com>
2016-01-29 17:12:36 +0200

    2. Örneğin eklenmesi
    buton örneği
    2 buton tıklayınca yazı değiştirme ve tıklayınca form rengi değiştirme

---
+++ Ornekler/2-Buton/2-Buton/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Bu yazı ile değişecek"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Form.ActiveForm.BackColor = Color.Red
+    End Sub
+End Class
--- Ornekler/README.md
+++ Ornekler/README.md
@@ -1,6 +1,6 @@
 # VB-Windows-Form-App Örnekleri
 
 - 1-Form giriş "Bir yazı bir form"
-- 2-Buton "Bildiğin buton eklenmiş form"
+- 2-Buton "Bildiğin buton eklenmiş form" Butona basınca yazı değiştirme
 - 3-List box ve buton "List box a buton ile data ekleme data gösterme"
 - 4-2 input içine sayı yazdırıp label de sonuçu göstermek.
