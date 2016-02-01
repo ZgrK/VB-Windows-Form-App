bf44f752bda19fe946565a28e69416a43c375662
Özgür KARALAR <zgr@zgrkaralar.com>
2016-02-01 21:01:38 +0200

    ornek 3
    Klasık bir sınav sorusu örneğidir

---
+++ Ornekler/3-List-Box/3-List-Box/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim value As Integer = CInt(Int((20 * Rnd()) + 1))
+        Label2.Text = value
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ListBox1.Items.Clear()
+        For i As Integer = 1 To Label2.Text
+            ListBox1.Items.Add(i)
+        Next i
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Clear()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Remove(ListBox1.SelectedItem)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim totalnum As Int32 = ListBox1.Items.Cast(Of Int32)().Sum()
+        Label3.Text = totalnum
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+
+        System.Diagnostics.Process.Start(e.Link.LinkData.ToString())
+    End Sub
+End Class
