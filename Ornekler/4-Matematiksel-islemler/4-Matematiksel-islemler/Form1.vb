5fdfe679c4df37d32c42b9402bc3b50f3658c39a
Özgür KARALAR <zgr@zgrkaralar.com>
2016-02-03 00:06:38 +0200

    4. Örnek
    Matematiksel işlemler

---
+++ Ornekler/4-Matematiksel-islemler/4-Matematiksel-islemler/Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text.Length > 0 And TextBox2.Text.Length > 0 Then
+
+            Dim sayi1, sayi2, sonuc As Integer
+            sayi1 = TextBox1.Text
+            sayi2 = TextBox2.Text
+            sonuc = Val(TextBox1.Text) + Val(TextBox2.Text)
+            Label3.Text = sonuc
+
+        Else
+            MsgBox("Kutları doldurunuz")
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox1.Text.Length > 0 And TextBox2.Text.Length > 0 Then
+
+            Dim sayi1, sayi2, sonuc As Integer
+            sayi1 = TextBox1.Text
+            sayi2 = TextBox2.Text
+            sonuc = Val(TextBox1.Text) - Val(TextBox2.Text)
+            Label3.Text = sonuc
+
+        Else
+            MsgBox("Kutları doldurunuz")
+        End If
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Text.Length > 0 And TextBox2.Text.Length > 0 Then
+
+            Dim sayi1, sayi2, sonuc As Integer
+            sayi1 = TextBox1.Text
+            sayi2 = TextBox2.Text
+            sonuc = Val(TextBox1.Text) / Val(TextBox2.Text)
+            Label3.Text = sonuc
+
+        Else
+            MsgBox("Kutları doldurunuz")
+        End If
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If TextBox1.Text.Length > 0 And TextBox2.Text.Length > 0 Then
+
+            Dim sayi1, sayi2, sonuc As Integer
+            sayi1 = TextBox1.Text
+            sayi2 = TextBox2.Text
+            sonuc = Val(TextBox1.Text) * Val(TextBox2.Text)
+            Label3.Text = sonuc
+
+        Else
+            MsgBox("Kutları doldurunuz")
+        End If
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If TextBox1.Text.Length > 0 Then
+
+            Dim sayi1, sonuc As Integer
+            sayi1 = TextBox1.Text
+            sonuc = Val(TextBox1.Text) * Val(TextBox1.Text)
+            Label3.Text = sonuc
+
+        Else
+            MsgBox("1.sayıyı doldurunuz")
+        End If
+    End Sub
+End Class
