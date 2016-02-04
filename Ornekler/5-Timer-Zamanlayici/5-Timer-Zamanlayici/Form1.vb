a2089da45f2f870c65bee4c86cc2b7bd4329c5fa
Özgür KARALAR <zgr@zgrkaralar.com>
2016-02-04 22:46:01 +0200

    Örnek 5 timer
    Zamanlayıcı ornegıdır

---
+++ Ornekler/5-Timer-Zamanlayici/5-Timer-Zamanlayici/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.Increment(1)
+        If Me.ProgressBar1.Value = 100 Then
+            Me.Timer1.Enabled = False
+            MsgBox("Bitiş")
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Enabled = False
+    End Sub
+End Class
